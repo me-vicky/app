27172a85534702aaf6cd548bb80db554a8725a50
Vikas Naik <vikas.naik01@sap.com>
2018-05-18 23:50:41 +0530

    updated index.html style.css and script.js  file
    updated index.html style.css and script.js  file

---
+++ javascripts/script.js
@@ -0,0 +1,20 @@
+
+// Time Picker Initialization
+// $('#input_starttime').pickatime({});
+
+//calendar
+// $( document ).ready(function() {
+    // $('#dp3').datepicker();
+// });
+
+
+// $('#datepicker').datepicker({
+	// uiLibrary: 'bootstrap4'
+	// });
+					
+					
+					
+					
+$(document).ready(function(){
+	$('[data-toggle="popover"]').popover();   
+});
